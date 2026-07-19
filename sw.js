6f2568de9240c5ae2085ad7530b464cd6a97c33a
Alexander <ping@boldfac.es>
2026-07-19 14:26:55 +0800

    Menu bar fix, new purge function, foldable support

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'receiptsnap-v3';
+const CACHE = 'receiptsnap-v4';
 const SHELL = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
