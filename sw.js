20f0bbff34de2d7a1c983601de3c6ebe443fc7ef
Alexander <ping@boldfac.es>
2026-07-28 12:32:57 +0800

    Bug fix for date not read properly

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'receiptsnap-v8';
+const CACHE = 'receiptsnap-v9';
 const LIBS = 'receiptsnap-libs';
 const SHELL = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 const LIB_HOSTS = ['cdnjs.cloudflare.com', 'cdn.jsdelivr.net', 'alcdn.msauth.net', 'fonts.googleapis.com', 'fonts.gstatic.com'];
