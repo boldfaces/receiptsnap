c488c71cfd071fc92444fc5fb0315030e8af17c9
Alexander <ping@boldfac.es>
2026-08-08 16:13:40 +0800

    Drop autocap, detection fix, add crop editor

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'receiptsnap-v12';
+const CACHE = 'receiptsnap-v13';
 const LIBS = 'receiptsnap-libs';
 const SHELL = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 const LIB_HOSTS = ['cdnjs.cloudflare.com', 'cdn.jsdelivr.net', 'alcdn.msauth.net', 'fonts.googleapis.com', 'fonts.gstatic.com'];
